9ea8b5b701281d0b39311c404d3d6f62f2b4d303
unknownmode001 <143152913+unknownmode001@users.noreply.github.com>
2024-12-20 17:11:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Directory where M3U8 files are stored
 M3U8_DIRECTORY = 'm3u8_files'
 
-@apap.route('/m3u8/<filename>', methods=['GET'])
+@app.route('/m3u8/<filename>', methods=['GET'])
 def serve_m3u8_file(filename):
     return send_from_directory(M3U8_DIRECTORY, f"{filename}.m3u8")
 
