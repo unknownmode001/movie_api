2cee362a0b319ef457051f6d9d793d02fc73660a
unknownmode001 <143152913+unknownmode001@users.noreply.github.com>
2024-12-20 17:44:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ M3U8_DIRECTORY = 'm3u8_files'
 
 @app.route('/m3u8/<filename>', methods=['GET'])
 def serve_m3u8_file(filename):
-    return send_from_directory(M3U8_DIRECTORY,filename)
+    return send_from_directory(M3U8_DIRECTORY,filename,mimetype="application/vnd.apple.mpegurl")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
